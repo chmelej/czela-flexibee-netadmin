82650c0259d6ffccbe101f2b7203592f7f7d9965
Jan Chmelensky <jan.chmelensky@fcrmedia.com>
2020-12-16 21:32:59 +0100

    oprava preklepu

--- src/fixNetadminData.groovy
+++ src/fixNetadminData.groovy
@@ -3,5 +3,5 @@ import net.czela.common.Helper
 
 Sql sql = Helper.newSqlInstance("app.properties", this)
 
-sql.executeUpdate("UPDATE denik SET datum_date = STR_TO_DATE(datum, '%d.%m.%Y') WHERE datm_date is null AND datum rlike '^[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}'".toString())
+sql.executeUpdate("UPDATE denik SET datum_date = STR_TO_DATE(datum, '%d.%m.%Y') WHERE datum_date is null AND datum rlike '^[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}'".toString())
 sql.executeUpdate("UPDATE akce SET cena = '0' WHERE cena not rlike '^\\-?[0-9]+(.[0-9]+)?\$'".toString())
